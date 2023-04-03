bb82ec857ebaf23e7dac960c689fd4433c2cd8f0
Shruthi <shruthi.bhagwati@gmail.com>
2023-04-03 12:16:36 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to Mongo successfully");
+    })
+}
+
+module.exports=connectToMongo;
